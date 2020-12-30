f9f173f13965ce99cb758180fe3ded867581346f
Scott Gloyna <scott.gloyna01@gmail.com>
2020-12-30 19:40:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -63,7 +63,7 @@ def get_english_meaning(file_path, japanese_emote)
 
   libary.each do |name,language|
     if language.key(japanese_emote)
-      #binding.pry
+      binding.pry
       answer = language[:english]
       #binding.pry
     end
