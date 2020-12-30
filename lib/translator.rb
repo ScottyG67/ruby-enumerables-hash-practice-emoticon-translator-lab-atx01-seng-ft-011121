7efe9db3d30164329a2912b4081ebbcd314503b2
Scott Gloyna <scott.gloyna01@gmail.com>
2020-12-30 18:34:13 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,8 +6,22 @@ require 'pry'
 def load_library(file_path)
   # code goes here
   require 'yaml'
-  hash = YAML.load(File.open(file_path))
+  libary = YAML.load(File.open(file_path))
   binding.pry
+  
+  #requirements
+  # libary is hash of array key is name of emote and array is english then japanese emote
+  # need to convert to hash of hash. First hash has key of sting name (no conversion needed) and items of :english and :japanese
+  # second hash has key as defined above then item as string from the yaml Array
+  
+  #process
+  # for each key and item (these are arrays) in libary hash
+  # make new hash with new_key = key and two items :english and :japanese
+  #   for each index in item array
+  #   if index = 0
+  #   newhash[new_key][:english] = item_array(0)
+  
+  
   hash
 end
 
