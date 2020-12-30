961c72d22ca845a20c9306efa93aee460b12f77a
Scott Gloyna <scott.gloyna01@gmail.com>
2020-12-30 19:34:39 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -46,10 +46,11 @@ end
 
 def get_english_meaning(file_path, japanese_emote)
   # code goes here
+  
   libary = load_library(file_path)
   answer=nil
   japanese_emote = "☜(⌒▽⌒)☞"
-  
+  binding.pry
   libary.each do |name,language|
     if language.key(japanese_emote)
       #binding.pry
@@ -57,6 +58,7 @@ def get_english_meaning(file_path, japanese_emote)
       #binding.pry
     end
   end
+  
   binding.pry
   answer
 end
