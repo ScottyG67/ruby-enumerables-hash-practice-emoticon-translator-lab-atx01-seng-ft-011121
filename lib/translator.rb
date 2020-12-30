8af933f6596338f42845fab7f7a181e70dc55b8d
Scott Gloyna <scott.gloyna01@gmail.com>
2020-12-30 18:58:58 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -38,7 +38,7 @@ def load_library(file_path)
   libary
 end
 
-def get_japanese_emoticon(file_path,english_emote)
+def get_japanese_emoticon(file_path, english_emote)
   # code goes here
   load_library(file_path)
   binding.pry
