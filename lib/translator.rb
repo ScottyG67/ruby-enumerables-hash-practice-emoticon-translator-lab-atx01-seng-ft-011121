379f7d805d8c74ef66400a618a373fc250dcf2db
Scott Gloyna <scott.gloyna01@gmail.com>
2020-12-30 18:25:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,7 +6,7 @@ require 'pry'
 def load_library(file_path)
   # code goes here
   require 'yaml'
-  YAML.load(File.open(file_path))
+  hash = YAML.load(File.open(file_path))
 end
 
 def get_japanese_emoticon
